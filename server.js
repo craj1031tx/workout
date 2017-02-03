653ff4ecd25519d9ceef5045ae497ddc368aebfe
craj1031tx <craj1031tx@gmail.com>
2017-02-03 17:02:51 -0600

    added bootstrap and sources in index.html with container

--- server.js
+++ server.js
@@ -18,8 +18,6 @@ require("./server/controllers/userSSController.js");
 require("./server/controllers/exerciseSSController.js");
 require("./server/configs/routes.js")(app);
 
-
-
 app.listen(8000, function(){
 	console.log("Now listening on port 8000");
 })
